37149bc486c2730dfa56250a1e033a3137f0d2a8
KavitaBrahmankar <kavitabrahmankar@gmail.com>
2021-10-18 09:12:31 +0530

    initial commit with first two test cases

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,16 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
+    public static int add(String input) {
+    	if(input.isEmpty()) {
+    		return 0;
+    	}else if(input.contains(",")) {
+    		String[] data = input.split(",");
+    		return Integer.parseInt(data[0]) +Integer.parseInt(data[1]);
+    	}else {
+    		return Integer.parseInt(input);
+    	}
+        
     }
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -8,13 +8,19 @@ class StringCalculatorShould {
 
     @Test
     void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+        assertEquals(0, StringCalculator.add(""));
     }
 
     @Test
     void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+        assertEquals(1, StringCalculator.add("1"));
     }
+    
+    @Test
+    public void sum_of_string_with_two_numbers_seperatedBy_comma() {
+    	assertEquals(3, StringCalculator.add("1,2"));
+    }
+    
+    
+    
 }
